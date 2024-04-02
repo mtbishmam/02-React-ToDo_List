0aa8b3b4fa1cb67a45c490384e9220e78cfeadc2
mtbishmam <mtbishmam@gmail.com>
2024-04-03 05:35:57 +0600

    Basic html & css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor='new-item'>New Item</label>
+          <input type="text" id="new-item" />
+        </div>
+        <button className='btn'>Add</button>
+    </form>
+    <h1 className='header'>To-Do List</h1>
+    <ul className='list'>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className='btn btn-danger'>Delete</button>
+      </li>
+    </ul>
+    </>
   )
 }
 
